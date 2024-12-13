be7faf69538616f47995e2e272bdea0dd005f464
sairam GitHub <skothi2022@my.fit.edu>
2024-12-13 12:33:33 -0500

    Added readme file

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 const {config} = require('./config/config');
 
-
+// Connecting to the configured port
 app.listen(config.server.port, () => {
     console.log(`Server is running on http://localhost:${config.server.port}`);
 });
